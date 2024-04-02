65cb69cb85fbe786d46cf97bc505814226762b7c
ro-el-c <chung3286@naver.com>
2024-04-02 16:17:30 +0900

    feat: week5 JavaScript 기초
    버튼 클릭시 더하기 계산 함수 작성

---
+++ week5~/playjs.js
@@ -0,0 +1,6 @@
+function calc() {
+  var x = parseInt(document.getElementById("x").value);
+  var y = parseInt(document.getElementById("y").value);
+  var result = document.getElementById("sum");
+  result.value = x + y;
+}
