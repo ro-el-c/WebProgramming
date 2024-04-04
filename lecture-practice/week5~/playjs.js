c08bb99a034e6b792d068bd955a8b40e31ec2fe2
ro-el <chung3286@naver.com>
2024-04-04 15:18:35 +0900

    feat: week5 구구단표, 숫자 맞추기 게임 구현
    JS 이중 반복문, 함수

--- lecture-practice/week5~/playjs.js
+++ lecture-practice/week5~/playjs.js
@@ -4,3 +4,39 @@ function calc() {
   var result = document.getElementById("sum");
   result.value = x + y;
 }
+
+
+function getRandomNum1to100 () {
+  return Math.floor(Math.random() * 100 + 1);
+}
+
+var computerNumber = getRandomNum1to100();
+var nGuesses = 0;
+
+function guess() {
+  nGuesses++;
+
+  var userNum = parseInt(document.getElementById("user").value);
+  var guessNum = document.getElementById("guesses");
+  var hint = document.getElementById("result");
+
+  guessNum.value = nGuesses;
+  
+  if (userNum == computerNumber) {
+    hint.value = "정답입니다.";
+  }
+  else if (userNum < computerNumber) {
+    hint.value = "낮습니다.";
+  }
+  else {
+    hint.value = "높습니다.";
+  }
+}
+
+function replay() {
+  computerNumber = getRandomNum1to100();
+  nGuesses = 0;
+  document.getElementById("guesses").value = 0;
+  document.getElementById("user").innerHTML = "";
+  document.getElementById("computer-num").value = computerNumber;
+}
